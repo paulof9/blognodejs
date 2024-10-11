16175d4a8a71275253aca833c51625bba4be2cfa
paulof9 <above2@outlook.com>
2024-10-11 15:23:13 -0300

    updates routes after new category

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 //Carregando módulos
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-const app = express();
-import { engine } from "express-handlebars";
-import admin from './routes/admin.js';
+    import express from 'express';
+    import path from 'path';
+    import { fileURLToPath } from 'url';
+    import { dirname } from 'path';
+    const app = express();
+    import { engine } from "express-handlebars";
+    import admin from './routes/admin.js';
+    import session from "express-session";
+    import flash from "connect-flash";
 
 //Configurações
+    //Sessão
+    app.use(session({
+        secret: "qualquercoisa",
+        resave: true,
+        saveUninitialized: true
+    }))
+    //Flash
+    app.use(flash());
+    //Middleware
+    app.use((req, res, next) => {
+        res.locals.success_msg = req.flash("success_msg");
+        res.locals.error_msg = req.flash("error_msg");
+        next();
+    })
+    //Express
     app.use(express.json());
     app.use(express.urlencoded({extended: true}))
     //Handlebars
@@ -18,6 +35,7 @@ import admin from './routes/admin.js';
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = dirname(__filename);
     app.use(express.static(path.join(__dirname,"public"))) //arquivos estaticos
+
     //MongooseDB
     import mongoose from "mongoose"
     mongoose.Promise = global.Promise;
--- routes/admin.js
+++ routes/admin.js
@@ -20,16 +20,31 @@ router.get('/categorias/add', (req, res) => {
 
 //cadastrar dados do form para a DB
 router.post('/categorias/nova', (req, res) => {
+        var erros = [];
+
+        if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+            erros.push({texto: "Nome inválido"}); //.push insere dados dentro de array
+        }
+        if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+            erros.push({texto: "Slug inválido"});
+        }
+        if(req.body.nome.length < 2){
+            erros.push({texto: "Nome da categoria muito pequeno, deve ser maior que dois dígitos!"})
+        }
+        if(erros.length > 0){
+            res.render("admin/addcategorias", {erros: erros})
+        }else{
     const novaCategoria = {
         nome: req.body.nome,
         slug: req.body.slug
     }
     new Categoria(novaCategoria).save().then(() => {
-        console.log("Categoria salva com sucesso!")
+        req.flash("success_msg", "Categoria criada com sucesso!");
+        res.redirect("/admin/categorias")
     }).catch((err) => {
-        console.log("Erro ao salvar categoria!"+err)
+        req.flash("error_msg", "Houve um erro ao salvar a categoria, tente novamente!")
+        res.redirect("/admin/categorias/add")
     })
-});
-
+}});
 // router
-export default router;
+export default router
